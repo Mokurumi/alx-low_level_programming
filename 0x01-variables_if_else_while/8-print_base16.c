c861fb13c682678c1fce7b8f03e35dc2f755b8e9
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-17 14:49:02 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char j
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+
+for (j = a; j <= 'f; j++)
+{
+putchar(j);
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
