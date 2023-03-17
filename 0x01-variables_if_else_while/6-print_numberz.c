dbca50a4a99bbb3bd0f2a6264f79b096e62344fd
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-17 14:37:40 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
