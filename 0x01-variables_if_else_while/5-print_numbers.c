a78989011d44e254b0c11ceadecd72092294fe97
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-17 14:24:18 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+{
+putchar(i);
+}
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
