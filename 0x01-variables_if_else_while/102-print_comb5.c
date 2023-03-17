8e523abb42e9a35c5626ff95f82b96235b20972a
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-17 16:53:59 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int i;
+int j;
+int k;
+
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+for (k = j + 1; k <= '9'; k++)
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(i);
+putchar(k);
+if (i == '9' && k = '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
