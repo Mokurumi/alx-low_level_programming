b72d7657fe9dfc0cb7ea25af0023bac750f3f599
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-17 17:49:00 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,9 +19,11 @@ for (i = 0; i <= 98; i++)
 for (j = i + 1; j <= 99; j++)
 {
 
-putchar(i);
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar(j);
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 
 if (i == 98 && j == 99)
 continue;
