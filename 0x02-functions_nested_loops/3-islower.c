91d65d3b1edb42801eb3a7f224d73bf42d8bf81f
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 13:07:00 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _islower - Entry point
+ */
+
+int _islower(int c)
+{
+
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
