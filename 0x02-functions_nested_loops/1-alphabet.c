a516115b5bca25647bdd70d2d70bc1f1a78fed79
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 12:19:26 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(){
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+
+_putchar('\n');
+
+}
+
+int main(void)
+{
+
+print_alphabet_x10();
+
+/* your code goes there */
+return (0);
+}
