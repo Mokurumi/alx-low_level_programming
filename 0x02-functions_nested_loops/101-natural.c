ba036bd39c03588c8cfd9352b0acb8210c6cbe0c
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 16:53:23 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Prints putchar
+ *
+ * Return: always 0
+ *
+ */
+
+void main(void)
+{
+
+int i;
+int sum;
+
+sum = 0;
+
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+sum += i;
+}
+
+printf("%d\n", sum);
+
+return (0);
+
+}
