1dd3de4c49af9982fcef3be785732180b4035704
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 14:50:51 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Prints putchar
+ */
+
+void times_table(void)
+{
+
+int i;
+int j;
+int p;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+
+_putchar('0' + ((i * j));
+_putchar(',');
+
+if (j < 10)
+_putchar(' ');
+
+_putchar(' ');
+
+}
+
+_putchar('\n');
+}
+
+}
