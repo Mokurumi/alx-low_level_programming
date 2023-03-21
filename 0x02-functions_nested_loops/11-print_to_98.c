e1cbbe39ab86195ff1e72130e8f4f746448dd7b1
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 15:50:54 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints putchar
+ *
+ * @n: input
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+
+while (n <= 98)
+{
+
+printf("%d, ", n);
+
+n++;
+}
+
+while (n >= 98)
+{
+printf("%d, ", n);
+n--;
+}
+
+printf("\n");
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,6 @@ int print_last_digit(int);
 void jack_bauer();
 void times_table();
 int add(int a, int b);
+void print_to_98(int n);
 
 #endif
