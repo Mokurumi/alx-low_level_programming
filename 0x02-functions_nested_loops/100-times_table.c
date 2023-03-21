8bccbacf5c57ef91b049dac3cf1c8005a2950d24
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 16:06:51 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints putchar
+ *
+ * @n: input
+ */
+
+void print_times_table(int n)
+{
+
+int i;
+int j;
+int p;
+
+while (n > -1 && n < 16)
+{
+
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+
+p = i * j;
+
+if (j != 0)
+{
+_putchar(',');
+
+if (p < 10)
+_putchar(' ');
+
+_putchar(' ');
+
+}
+
+if (p < 10)
+{
+_putchar('0' + p);
+}
+else
+{
+_putchar('0' + (p / 10));
+_putchar('0' + (p % 10));
+}
+
+}
+
+_putchar('\n');
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,6 @@ void jack_bauer();
 void times_table();
 int add(int a, int b);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
