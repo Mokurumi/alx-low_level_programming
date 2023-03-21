33a7f7537a51fa014d0aa8704a90637e4a5456d7
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 12:54:52 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet - Entry point
+ */
+
+void print_alphabet_x10(void)
+{
+
+char i;
+
+for (k = 0; k < 10; k++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+}
+
+_putchar('\n');
+
+}
