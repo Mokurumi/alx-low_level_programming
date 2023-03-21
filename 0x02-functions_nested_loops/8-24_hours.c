13092cfb668337a1d3c047ac9d2bdc81a97f9ef4
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 14:24:44 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints putchar
+ */
+
+void jack_bauer(void)
+{
+
+int h;
+int m;
+
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+if (h < 10)
+{
+_putchar(0);
+}
+
+_putchar(':');
+_puchar(h);
+
+if (m < 10)
+{
+_putchar(0);
+}
+
+_putchar(m);
+_putchar('\n');
+}
+}
+
+}
