b12c77906f36510e1116f759ef420d33611c903e
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 18:11:43 -0700

    fibonacci 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,8 +11,10 @@ int main(void)
 int sum = 0;
 int first = 1, second = 2, next;
 
-while (second <= 4000000) {
-if (second % 2 == 0) {
+while (second <= 4000000)
+{
+if (second % 2 == 0)
+{
 sum += second;
 }
 next = first + second;
