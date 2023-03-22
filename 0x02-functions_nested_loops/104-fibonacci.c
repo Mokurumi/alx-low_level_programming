a3273445987119b63315826e77f584c127934ce1
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 18:18:36 -0700

    fibonacci 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,8 +6,9 @@
  * Return: always 0
  *
  */
-int main()
+int main(void)
 {
+
 int a = 1, b = 2, next, i;
 
 printf("%d, %d", a, b);
@@ -15,6 +16,9 @@ printf("%d, %d", a, b);
 for (i = 2; i < 98; i++)
 {
 next = a + b;
+if (next < 0) {
+break;
+}
 printf(", %d", next);
 a = b;
 b = next;
