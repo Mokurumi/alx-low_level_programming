9d8dac233ecbefe09dabba662d5e2bde0cf48ad9
ALpha Mokurumi <mokurumialpha@gmail.com>
2023-03-21 17:04:03 -0700

    fibonacci count

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+/**
+ * main - main function
+ *  
+ * Return: always 0
+ */
+
+int main(void)
+{
+      
+int n = 50;
+int first = 1;
+int second = 2;
+int next;
+int i;
+
+printf("%d, %d, ", first, second); // print first two numbers
+
+for (i = 2; i < n; i++)
+{
+next = first + second;
+printf("%d", next);
+
+if (i != n - 1)
+{
+printf(", ");
+}
+
+first = second;
+second = next;
+}
+
+printf("\n");
+
+return 0;
+
+}
