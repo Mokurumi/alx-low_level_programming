c52b48f8d8275de9c01065b0cfac1d5a0c89ab66
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-03-22 23:16:46 -0700

    Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
+int _putchar(char ch);
+void print_alphabet();
+void print_alphabet_x10();
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer();
+void times_table();
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
