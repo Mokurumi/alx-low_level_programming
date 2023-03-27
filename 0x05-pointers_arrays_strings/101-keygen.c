8a8aa5a91944ec359fa86666c033ed7fd4f99bbc
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-03-27 13:52:15 -0700

    strcpy
    strcpy
    
    strcpy
    
    strcpy
    
    extra

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]\|;:',./<>?~`";
+	const size_t charset_size = sizeof(charset) - 1;
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int index = rand() % charset_size;
+		password[i] = charset[index];
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - check the code
+ *
+ * @dest: input
+ * @src: input
+ *
+ * Return: char output
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
