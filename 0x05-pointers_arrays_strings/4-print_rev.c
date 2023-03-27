e712c2f0dfeafa9996b0b3807bd095d46e754a84
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-03-27 11:47:49 -0700

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ * @str: input
+ *
+ */
+void print_rev(char *str)
+{
+	int i;
+	int n -_strlen(str);
+
+	for (i = (n - 1); i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
