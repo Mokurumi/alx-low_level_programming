1f137b8157f48f8d0e510c84b3dc32921539a628
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-03-27 12:15:22 -0700

    skip indices

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * @str: input
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+	int n = strlen(str);
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+		i++
+	}
+
+	_putchar('\n');
+}
