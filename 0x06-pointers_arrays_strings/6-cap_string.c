3555d82a9e320d39fa9d19a8619aae6fcd7e5fec
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-03-30 18:24:35 -0700

    q6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ *
+ * @n: input
+ *
+ * Return: char val
+ */
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if ((n[i - 1] == ' ' || n[i - 1] == '\n' || n[i - 1] == '\t' || n[i - 1] == ','
+					|| n[i - 1] == ';' || n[i - 1] == '!' || n[i - 1] == '?'
+					|| n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')'
+					|| n[i - 1] == '{' || n[i - 1] == '}' || n[i - 1] == '.')
+				&& (n[i] >= 'a' && n[i] <= 'z'))
+		{
+			n[i] = n[i] - 32;
+		}
+		else if (i == 0 && n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		else
+		{
+			n[i] = n[i];
+		}
+
+		i++;
+	}
+
+	return (n);
+}
