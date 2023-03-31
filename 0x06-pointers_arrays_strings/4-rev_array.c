95e9c6ddc91ce8df3d786df3e34639d2c13382ee
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-03-30 18:16:34 -0700

    q4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,14 +11,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int *tmp = malloc(n * sizeof(int));
+	int tmp;
 
 	for (i = 0; i < n / 2; i++)
 	{
-		tmp[i] = a[i];
+		tmp = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = tmp[i];
+		a[n - 1 - i] = tmp;
 	}
-
-	free(tmp);
 }
