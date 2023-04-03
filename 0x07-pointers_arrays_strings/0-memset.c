88c2eb83459cbdcc6de14b08e02e7023edf60e13
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-03 15:22:46 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - check the code
+ *
+ * @s: input
+ * @b: input
+ * @n: input
+ *
+ * Return: char
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return s;
+}
