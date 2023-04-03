8e4d6d69e1285402dff70d2fe32aa7e55910092e
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-03 15:49:03 -0700

    The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - check the code
+ *
+ * @a: input
+ * @size: input
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		/* add diagonal element */
+		sum1 += *(a + i*size + i);
+		/* add anti-diagonal element */
+		sum2 += *(a + i*size + size - 1 - i);
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
