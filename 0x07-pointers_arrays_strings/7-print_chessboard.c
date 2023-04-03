57e8fc673628e66151a5bcf6362ae4c3e428f8be
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-03 15:45:11 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - check the code
+ *
+ * @a: input
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	while (i < 8)
+	{
+		int j = 0;
+
+		while (j < 8)
+		{
+			printf("%c ", a[i][j]);
+			j++;
+		}
+
+		printf("\n");
+		i++;
+	}
+}
