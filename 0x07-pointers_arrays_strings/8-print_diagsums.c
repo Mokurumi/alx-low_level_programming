da3e33bee0b393aaa6915a615d1e3b0318eafb3d
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-03 15:59:51 -0700

    correct 8-print

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,10 +10,12 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		sum1 += *(a + i*size + i);
-		sum2 += *(a + i*size + size - 1 - i);
+		/* add diagonal element */
+		sum1 += *(a + i * size + i);
+		/* add anti-diagonal element */
+		sum2 += *(a + i * size + size - 1 - i);
 	}
 
 	printf("%d, %d\n", sum1, sum2);
