0ad18b442bf801ee3852293e9df0211904f93c15
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-05 19:47:13 -0700

    q8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,7 +10,7 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	return wildcmp_helper(s1, s2);
+	return (wildcmp_helper(s1, s2));
 }
 
 /**
@@ -23,31 +23,32 @@ int wildcmp(char *s1, char *s2)
  */
 int wildcmp_helper(char *s1, char *s2)
 {
+	int res;
+
 	/* If the strings are identical or s2 is empty and s1 is not */
 	if (*s1 == *s2)
 	{
 		if (*s1 == '\0')
-			return 1;
+			return (1);
 		else
-			return wildcmp_helper(s1+1, s2+1);
+			return (wildcmp_helper(s1 + 1, s2 + 1));
 	}
 	/* If s2 contains the special character * */
 	else if (*s2 == '*')
 	{
 		/* If the next character in s2 is also *, skip it */
-		while (*(s2+1) == '*')
-			s2++;
+		if (*(s2 + 1) == '*')
+			return (wildcmp_helper(s1, s2 + 1));
+
 		/* Check all possible combinations of s1 and s2 after the * */
-		while (*s1 != '\0')
-		{
-			if (wildcmp_helper(s1, s2+1))
-				return 1;
-			s1++;
-		}
-		/* If no match is found */
-		return 0;
+		res = wildcmp_helper(s1, s2 + 1);
+
+		if (!res && *s1 != '\0')
+			res = wildcmp_helper(s1 + 1, s2);
+
+		return (res);
 	}
 	/* If the chars do not match and s2 doesn't contain the special char * */
 	else
-		return 0;
+		return (0);
 }
