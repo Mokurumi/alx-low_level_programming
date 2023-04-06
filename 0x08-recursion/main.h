81fc9591a5a362ae5fe750a16553c0d8c7fca0c5
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-05 19:34:41 -0700

    mainh

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,7 +13,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int start);
 int is_prime_number(int n);
-int prime_helper(int n, int i)
+int prime_helper(int n, int i);
 int is_palindrome(char *s);
 int pali_helper(char *s, int start, int end);
 int wildcmp(char *s1, char *s2);
