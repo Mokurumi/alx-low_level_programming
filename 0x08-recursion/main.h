cecac48d10404ffd246007a87b6939db58a782e1
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-05 19:03:12 -0700

    vv

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int start);
 int is_prime_number(int n);
-int _prime_helper(int n, int i)
+int prime_helper(int n, int i)
 
 #endif
