95daf8f117634d2e634bb03c25c59619fa0bcbd8
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-17 15:31:19 -0700

    populate struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog
+ *
+ * @d: struct to initialize
+ * @name: val 1
+ * @age: val 2
+ * @owner: val 3
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
