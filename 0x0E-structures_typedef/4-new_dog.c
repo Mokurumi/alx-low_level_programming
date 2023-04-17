85fe6ab94ba9a5686184308b3f40545b6c42c11c
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-17 16:15:08 -0700

    create dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,17 +12,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d;
+	char *name_copy, *owner_copy;
 
 	d = malloc(sizeof(struct dog));
-
-	if (d != NULL)
+	if (d == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-
-		return (d);
+		return (NULL);
 	}
 
-	return (NULL);
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(name_copy, name);
+
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(owner_copy, owner);
+
+	d->name = name_copy;
+	d->age = age;
+	d->owner = owner_copy;
+
+	return (d);
 }
