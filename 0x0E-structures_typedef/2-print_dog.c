a805e8495c9614d35cca078400d7bc5549eb91f5
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-17 15:54:48 -0700

    structs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,13 +4,18 @@
  * print_dog - check the code
  *
  * @d: dog struct
+ *
+ * Return: nothing
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		return;
 	}
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+
 }
