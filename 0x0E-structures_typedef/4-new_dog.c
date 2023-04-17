55f18529d83ccf229fce36ef53a073c6450e4961
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-04-17 16:05:52 -0700

    create dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ * new_dog - create new dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	d = malloc(sizeof(struct dog));
+
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
+	return d;
+}
