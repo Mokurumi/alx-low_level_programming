0f028facad4d6f77949b6fafbba94c628e520785
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-05-08 18:44:31 -0700

    copy content btn files

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * print_usage_and_exit - Prints usage message and exits with error code 97.
+ */
+void print_usage_and_exit(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * print_error_and_exit - Prints error message with filename and exits
+ *
+ * @message: The error message to print.
+ * @filename: The name of the file associated with the error.
+ */
+void print_error_and_exit(const char *message, const char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
+	exit(errno);
+}
+
+/**
+ * copy_file - Copies the content of a file to another file.
+ *
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, read_chars, write_chars;
+	char buf[1024];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		print_error_and_exit("Can't read from file ", file_from);
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
+		print_error_and_exit("Can't write to ", file_to);
+
+	while ((read_chars = read(fd_from, buf, 1024)) > 0)
+	{
+		write_chars = write(fd_to, buf, read_chars);
+		if (write_chars != read_chars)
+			print_error_and_exit("Can't write to ", file_to);
+	}
+
+	if (read_chars == -1)
+		print_error_and_exit("Can't read from file ", file_from);
+
+	if (close(fd_from) == -1)
+		print_error_and_exit("Can't close fd ", file_from);
+
+	if (close(fd_to) == -1)
+		print_error_and_exit("Can't close fd ", file_to);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+		print_usage_and_exit();
+
+	copy_file(argv[1], argv[2]);
+
+	return (0);
+}
