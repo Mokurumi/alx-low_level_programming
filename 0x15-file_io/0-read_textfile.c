5fccfef66a0d11cebd369ea5297dec29a4d540e3
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-05-10 00:49:56 -0700

    correct read file
    correct read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,34 +10,44 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *filepointer;
+	int fd;
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
-	ssize_t bytes_read;
 
 	if (filename == NULL)
 		return (0);
 
-	filepointer = fopen(filename, "r");
-	if (filepointer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(letters + 1);
+	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 	{
-		fclose(filepointer);
+		close(fd);
 		return (0);
 	}
 
-	bytes_read = fread(buffer, 1, letters, filepointer);
-	fclose(filepointer);
-
-	if (bytes_read == 0 ||
-			fwrite(buffer, 1, bytes_read, stdout) != (size_t) bytes_read)
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
 	{
+		close(fd);
 		free(buffer);
 		return (0);
 	}
 
+	buffer[bytes_read] = '\0';
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
 	free(buffer);
-	return (bytes_read);
+
+	return (bytes_written);
 }
