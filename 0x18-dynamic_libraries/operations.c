a6730f9f630e3f671d2753dc0c2578a5fd05bf71
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-08-21 15:07:18 -0700

    c lib for py

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - add func
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub func
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply func
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div func
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod func
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
