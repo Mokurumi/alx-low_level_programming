325d51eceea6dee3a1402c25357575262505b7e9
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-10 13:57:43 -0700

    correct djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,13 +6,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int c;
+	unsigned long int hash = 5381;
 
-	hash = 5381;
-	while ((c = *str++))
+	while (*str)
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + *str; /* hash * 33 + c */
+		str++;
 	}
+
 	return (hash);
 }
